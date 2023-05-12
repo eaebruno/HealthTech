2fb4d70b90a3bb9fd3c338f9bb005d203b10e9d1
Bruno Moreira <99297172+eaebruno@users.noreply.github.com>
2023-05-12 10:35:41 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+hamburguer = document.querySelector(".menuhamburguer");
+
+hamburguer.addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu");
+});
+
